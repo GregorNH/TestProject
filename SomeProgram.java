411a4b6aa6ece864ec5bfbe624e1fb4a5e834f2d
Greg Kozak <qozak@wp.pl>
2018-01-30 21:13:42 +0100

    First implant

---
+++ SomeProgram.java
@@ -0,0 +1,9 @@
+public class SomeProgram {
+	public static void main(String[] args)
+	{
+	System.out.println("We are learning to use Git");
+	System.out.println("Soon to be GitExpert");
+	}		
+	
+	
+}
